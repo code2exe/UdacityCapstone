9236dab9bc579afdc95eb5c6de89efd19d97e5c6
Henry <ec2-user@ip-172-31-2-113.eu-west-2.compute.internal>
2020-06-05 15:00:27 +0000

    Final Commit

--- scripts/build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-## Complete the following steps to get Docker running locally
-
-# Step 1:
-# Build image and add a descriptive tag
-sudo docker build --tag=udacity-capstone .
